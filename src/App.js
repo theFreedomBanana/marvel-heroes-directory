7a5caed16bd65a9dcae737496e34ca8290c17cca
Ben <benjamin.mahmoudi@gmail.com>
2018-08-21 10:58:37 +0200

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react"
 import HeroesList from "./heroes/heroesList"
+import { BrowserRouter, Route } from "react-router-dom"
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HeroesList></HeroesList>
+        <BrowserRouter>
+          <Route to="/" render={ () => (
+            <HeroesList></HeroesList>
+          )} />
+        </BrowserRouter>
       </div>
     )
   }
