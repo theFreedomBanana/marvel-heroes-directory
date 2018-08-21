35ecc2dc6f0d7ec7ad5bafac16273f44e84d2291
Ben <benjamin.mahmoudi@gmail.com>
2018-08-21 17:08:48 +0200

    Add Links to hero img and name - Add overlay to hero img

--- src/heroes/heroThumbnail.jsx
+++ src/heroes/heroThumbnail.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
 import { FaLink } from "react-icons/fa"
 import "./heroThumbnail.css"
 
@@ -31,11 +32,14 @@ class HeroThumbnail extends Component<Props, {}> {
     return(
       <li className="col-sm-12 col-lg-4">
         <div className="hero-thumbnail-container">
-          <div className="thumbnail-img-container">
-            <img src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt={hero.name} />
-          </div>
+          <Link to={`/characters/${hero.id}`}>
+            <div className="overlay"></div>
+            <div className="thumbnail-img-container">
+              <img src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt={hero.name} />
+            </div>
+          </Link>
           <div className="thumbnail-infos-container">
-            <h4>{hero.name}</h4>
+            <Link to={`/characters/${hero.id}`}><h4>{hero.name}</h4></Link>
             <div className="links-container">
               { hero.urls[0] ? <a href={hero.urls[0].url} target="_blank"><FaLink />details</a> : null }
               { hero.urls[1] ? <a href={hero.urls[1].url} target="_blank"><FaLink />wiki</a> : null }
